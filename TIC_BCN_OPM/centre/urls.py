df66dfe11a66f6562fa49381d83340b0a646a40d
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-03-08 16:00:07 +0100

    Creades views index, students i teachers, i fet les urls

---
+++ TIC_BCN_OPM/centre/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('teachers', views.teachers, name='teachers'),
+    path('students', views.students, name='students')
+]
--- TIC_BCN_OPM/centre/views.py
+++ TIC_BCN_OPM/centre/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def teachers(request):
+    return render(request, 'teachers.html')
+
+def students(request):
+    return render(request, 'students.html')
